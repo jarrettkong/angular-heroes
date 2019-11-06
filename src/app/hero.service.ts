1b0edaea78610b045df20a19aa2d4b6341301dfc
Jarrett Kong <jarrettkong@me.com>
2019-11-06 11:25:38 -0700

    Refactor hero service to use HttpClient fetch from in memory data instead of using mock data
    Signed-off-by: Jarrett Kong <jarrettkong@me.com>

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable, of } from 'rxjs'
+import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero'
-import { HEROES } from './mock-heroes'
 import { MessageService } from './message.service'
 
 @Injectable({
@@ -9,15 +10,35 @@ import { MessageService } from './message.service'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl: string = 'api/heroes'
+
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('Fetched heroes suceessfully');
-    return of(HEROES);
+    this.log('Fetched heroes suceessfully');
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    )
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(`${this.heroesUrl}/${id}`).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
